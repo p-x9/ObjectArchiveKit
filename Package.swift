e3db5f43ed0fb1d5e87969529cae89e1805c5c4d
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-03-07 04:03:49 +0900

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -10,10 +10,34 @@ let package = Package(
             targets: ["ObjectArchiveKit"]
         ),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/p-x9/swift-fileio.git",
+            from: "0.13.0"
+        ),
+        .package(
+            url: "https://github.com/p-x9/swift-fileio-extra.git",
+            from: "0.2.2"
+        ),
+        .package(
+            url: "https://github.com/p-x9/swift-binary-parse-support.git",
+            from: "0.2.1"
+        ),
+    ],
     targets: [
         .target(
-            name: "ObjectArchiveKit"
+            name: "ObjectArchiveKit",
+            dependencies: [
+                "ObjectArchiveKitC",
+                .product(name: "FileIO", package: "swift-fileio"),
+                .product(name: "FileIOBinary", package: "swift-fileio-extra"),
+                .product(
+                    name: "BinaryParseSupport",
+                    package: "swift-binary-parse-support"
+                )
+            ]
         ),
+        .target(name: "ObjectArchiveKitC"),
         .testTarget(
             name: "ObjectArchiveKitTests",
             dependencies: ["ObjectArchiveKit"]
