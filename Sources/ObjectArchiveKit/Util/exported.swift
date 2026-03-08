9b0b7d7362be9079fb839b317f93c47c19ec03ac
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-03-08 21:59:37 +0900

    exported import

---
+++ Sources/ObjectArchiveKit/Util/exported.swift
@@ -0,0 +1,13 @@
+//
+//  exported.swift
+//  ObjectArchiveKit
+//
+//  Created by p-x9 on 2026/03/07
+//  
+//
+
+@_exported
+import ObjectArchiveKitC
+
+@_exported
+@_spi(Core) import BinaryParseSupport
