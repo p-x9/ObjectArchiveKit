59407147ffb012ab4307891390709d35041bc125
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-03-13 02:37:21 +0900

    Add test cases

--- Tests/ObjectArchiveKitTests/ObjectArchiveKitTests.swift
+++ Tests/ObjectArchiveKitTests/ObjectArchiveKitTests.swift
@@ -1,4 +1,58 @@
 import XCTest
 @testable import ObjectArchiveKit
 
-final class ObjectArchiveKitTests: XCTestCase {}
+final class ObjectArchiveKitTests: XCTestCase {
+
+    private var archive: ArchiveFile!
+
+    override func setUp() {
+        let path = ""
+        let url = URL(fileURLWithPath: path)
+        archive = try! ArchiveFile(url: url)
+    }
+}
+
+extension ObjectArchiveKitTests {
+    func testArchive() {
+        print("Magic:", archive.magic)
+        print("Kind:", archive.kind)
+    }
+
+    func testMembers() {
+        for (i, member) in archive.members.enumerated() {
+            print("[\(i)]", member.name(in: archive))
+            print(" offset", member.offset)
+            print(" size", member.header.size)
+        }
+    }
+}
+
+extension ObjectArchiveKitTests {
+    func testGNUSymbols() throws {
+        guard let symbolTable = archive.gnuSymbolTable else {
+            return
+        }
+        print("count: \(symbolTable.count)")
+        if let offsets = symbolTable.offsets(in: archive) {
+            print("offset:")
+            for (i, offset) in offsets.enumerated() {
+                print("", i, offset)
+            }
+        }
+        if let names = try symbolTable.names(in: archive) {
+            print("names:")
+            for (i, name) in names.enumerated() {
+                print("", i, name.string)
+            }
+        }
+    }
+
+    func testGNUStrings() {
+        guard let strings = archive.gnuStrings else {
+            return
+        }
+        for string in strings {
+            print(string.offset, string.string)
+        }
+    }
+}
